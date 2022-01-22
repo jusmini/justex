dbe8c90cedbbbe8f5517b8fd4827e82fa0545bb3
Justyna Min <just.minias@gmail.com>
2022-01-22 15:27:13 +0100

    testowy

--- 0101new1.py
+++ 0101new1.py
@@ -1,9 +1,11 @@
-number = int(input("Podaj wielokrotność znaków: "))
+# number = int(input("Podaj wielokrotność znaków: "))
+#
+# counter = 1
+#
+# for i in range(1, number + 1):
+#     for x in range(0, counter):
+#         print("#", end="")
+#     counter += 1
+#     print()
 
-counter = 1
-
-for i in range(1, number + 1):
-    for x in range(0, counter):
-        print("#", end="")
-    counter += 1
-    print()
+xxx
