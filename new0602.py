0b14aa9360506b65528c1341acf71be9294ad4a5
Justyna Min <just.minias@gmail.com>
2022-02-09 20:45:57 +0100

    Commit zadań z 09.02

--- new0602.py
+++ new0602.py
@@ -18,22 +18,81 @@
 #
 # print(choose_best_sum(max_dis, city, distances))
 
-from collections import Counter
+# from collections import Counter
+#
+# x = ["NORTH", "WEST", "SOUTH", "EAST"]
+#
+#
+# def dirReduc(arr: list):
+#     directions = dict(Counter(arr))
+#     north = directions.get('NORTH', 0)
+#     south = directions.get('SOUTH', 0)
+#     east = directions.get('EAST', 0)
+#     west = directions.get('WEST', 0)
+#
+#     temp_1 = ['NORTH'] * (north - south) if north > south else ['SOUTH'] * (south - north)
+#     temp_2 = ['EAST'] * (east - west) if east > west else ['WEST'] * (west - east)
+#
+#     return temp_1 + temp_2
+#
+#
+# print(dirReduc(x))
 
-x = ["NORTH", "WEST", "SOUTH", "EAST"]
+
+# x = 120
+#
+#
+# def make_readable(seconds: int):
+#     if 359999 >= seconds > 0:
+#         hours = seconds//3600
+#         difference = seconds - (hours * 3600)
+#         minutes = difference//60
+#         seconds = difference - (minutes * 60)
+#
+#         if hours <= 9:
+#             hours = '0' + str(hours)
+#         if minutes <= 9:
+#             minutes = '0' + str(minutes)
+#         if seconds <= 9:
+#             seconds = '0' + str(seconds)
+#
+#         return f'{hours}:{minutes}:{seconds}'
+#     else:
+#         return '00:00:00'
+#
+#
+# print(make_readable(x))
 
 
-def dirReduc(arr: list):
-    directions = dict(Counter(arr))
-    north = directions.get('NORTH', 0)
-    south = directions.get('SOUTH', 0)
-    east = directions.get('EAST', 0)
-    west = directions.get('WEST', 0)
-
-    temp_1 = ['NORTH'] * (north - south) if north > south else ['SOUTH'] * (south - north)
-    temp_2 = ['EAST'] * (east - west) if east > west else ['WEST'] * (west - east)
-
-    return temp_1 + temp_2
+# x = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+# x = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+x = [[3, 6, 9, 12], [2, 5, 8, 11], [1, 4, 7, 10]]
+y = 'clockwise'
 
 
-print(dirReduc(x))
+def rotate(matrix: list, direction: str):
+    frame = []
+    for sub_list in range(0, len(matrix[0])):
+        frame.append([0] * len(matrix))
+
+    if direction == 'clockwise':
+        destination_column = len(matrix)-1
+        start_row = 0
+        row_increment = 1
+        column_increment = -1
+    else:
+        destination_column = 0
+        start_row = len(matrix[0])-1
+        row_increment = -1
+        column_increment = 1
+
+    for sub_list in matrix:
+        row = start_row
+        for element in sub_list:
+            frame[row][destination_column] = element
+            row += row_increment
+        destination_column += column_increment
+
+    return frame
+
+print(rotate(x, y))
